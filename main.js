0e295d5cf38674d0cffec1dd81b426720db7b932
CarrotBRRR <69835046+CarrotBRRR@users.noreply.github.com>
2025-08-22 03:29:23 -0600

    Added SPI to main.js file

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 // List of markdown files to load and render
 const markdownFiles = [
-  { file: './markdown-files/test.md', title: 'TeStinG' },
-  { file: './markdown-files/another-test.md', title: 'T E S T 2' },
+  { file: './markdown-files/SPI.md', title: 'SPI' },
   // ADD NEW MARKDOWN FILES HERE
 ];
 
