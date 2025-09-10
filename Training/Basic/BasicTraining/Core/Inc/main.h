449d51ed002b96de6e82dd7aaeef415755d5ef4f
MacKante <mackante.git@gmail.com>
2025-09-10 10:31:11 -0600

    Added basic training project and gitignore

---
+++ Training/Basic/BasicTraining/Core/Inc/main.h
@@ -0,0 +1,89 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32h5xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define LED0_Pin GPIO_PIN_13
+#define LED0_GPIO_Port GPIOC
+#define ACC_INT2_Pin GPIO_PIN_7
+#define ACC_INT2_GPIO_Port GPIOA
+#define ACC_INT2_EXTI_IRQn EXTI7_IRQn
+#define ACC_INT1_Pin GPIO_PIN_0
+#define ACC_INT1_GPIO_Port GPIOB
+#define ACC_INT1_EXTI_IRQn EXTI0_IRQn
+#define IOX_nRESET_Pin GPIO_PIN_1
+#define IOX_nRESET_GPIO_Port GPIOB
+#define IOX_INTB_Pin GPIO_PIN_2
+#define IOX_INTB_GPIO_Port GPIOB
+#define IOX_INTB_EXTI_IRQn EXTI2_IRQn
+#define IOX_INTA_Pin GPIO_PIN_10
+#define IOX_INTA_GPIO_Port GPIOB
+#define IOX_INTA_EXTI_IRQn EXTI10_IRQn
+#define SPI2_CS_IOX_Pin GPIO_PIN_12
+#define SPI2_CS_IOX_GPIO_Port GPIOB
+#define SPI2_CS0_Pin GPIO_PIN_8
+#define SPI2_CS0_GPIO_Port GPIOA
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
